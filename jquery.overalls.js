74c3fae009a08f9270c38b513de8354d05c1412e
Tyler Montgomery <tyler.a.montgomery@gmail.com>
2010-03-14 23:51:51 -0600

    still working out the kinks, but almost done

--- jquery.overalls.js
+++ jquery.overalls.js
@@ -3,7 +3,7 @@
    http://github.com/ubermajestx/jquery.overalls
 */
 (function($) {
-  function Overalls(message){
+  function Overalls(html, opts){
       // alert('overalls: ' + message)
       // return false;
       // options for:
@@ -16,49 +16,111 @@
       //   page placement
       //   css-class
       //   
-      window_size = { width: $(window).width(), height: $(window).height()}
-      log(window_size)
       
+      
+      close_overalls(); //clear any existing overlays
+      z_index = max_z_index() //make sure Overalls is the highest z-index'd set of divs (avoids hardcoding rediculous z-index)
+      
+      // add transparency to 100% of the document
       transparency_css = {
-        position: 'absolute',
+        position: 'fixed',
         top: 0,
         left:0,
-        width: window_size.width,
-        height: window_size.height, 
-        opacity: 0.6,
-        background: '#1d1d1d'
+        width: '100%',
+        height: '100%',
+        opacity: 0.0, //option
+        zIndex: z_index,
+        background: '#1d1d1d' //option
       }
-      $('<div id="overalls-transparency" style=""></div>').css(transparency_css)
-      .click(close_overalls()).appendTo('body')
-      // add transparency to 100% of the document
-      // add overlay div to page
-      // 
-      // concerns: 
-      //   multiple overlays
-      //   scrolling
-      // 
       
+      transparency_div = $('<div id="overalls-transparency"></div>').css(transparency_css)
+      .click(function(){close_overalls()})
+      .appendTo('body')
+      
+      // make another completely transparent div to place the overly div inside of
+      super_transparent_css = {
+        position: 'absolute',
+        top:0,
+        left:0,
+        width: '100%',
+        height: '0',
+        zIndex: z_index + 1,
+        background: 'transparent'
+        // 'transparent'
+      }
+      super_transparent_div = $('<div id="overalls-super-transparent"></div>').css(super_transparent_css)
+      .appendTo('body')
+      
+      // add overlay div to page
+      overlay_css = {
+        background: '#fff',
+        color: '#1d1d1d',
+        height: '350px',//remove this to resize to the content
+        width: '650px',//remove this to resize to the content
+        padding: '20px',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        zIndex: z_index + 2,
+        opacity: 1.0
+      }
+      
+      overlay_div = $('<div id="overalls-overlay"></div>').css({marginTop:$('body').scrollTop() + 30})
+      if(opts.cssClass){
+        log('adding css class', opts.cssClass)
+        overlay_div.addClass(opts.cssClass)
+      }
+      else if(opts.blank == true){
+        // no-op
+      }
+      else{
+        overlay_div.css(overlay_css)
+      }
+      overlay_div.html(html)
+      .appendTo(super_transparent_div)
+      
+      $(transparency_div).fadeTo("slow", 0.6)
+      $(overlay_div).fadeTo("slow", 1.0)
+    
       document.onkeyup = (function(e) {
         if (e.keyCode == 27) {
           close_overalls();
         };
       });
       
-    function close_overalls(){
-      //TODO animate
-      $('div#overalls-transparency, div#overalls-overlay').remove
-    }  
+      overalls = this
+      overalls.close = close_overalls
+      
+      function close_overalls(){
+        log('taking the overalls off')
+        all_divs = $('div#overalls-transparency, div#overalls-super-transparent, div#overalls-overlay').fadeOut(600)
+        setTimeout(function(){all_divs.remove()},650)
+      }  
+      
+      function max_z_index(){
+        z = 0
+        $('*').each(function() {
+          log('.')
+          current = parseInt($(this).css('zIndex'));
+          z = current > z ? current : z; 
+        });
+        return z + 1;
+      }
+
+
+      function log() {
+          if (window.console) {
+              console.log.apply(console, arguments);
+          }
+      }
+      
+      
+    } // end of Overalls
+
     
-    
-    function log() {
-        if (window.console) {
-            console.log.apply(console, arguments);
-        }
-    }
-    
-    }
-  $.overalls = function(message){
-    var overalls = new Overalls(message)
+  $.overalls = function(html, opts){
+    if(!opts)
+      opts = {}
+    var overalls = new Overalls(html, opts)
     return overalls
   }
 })(jQuery);
